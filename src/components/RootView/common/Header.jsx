8f0c6fff631f1bbf7e0ade3f2c300d33efabe9f5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-16 21:59:19 +0600

    enhance MyCart component with product display and quantity controls; remove unused code from Header

--- src/components/RootView/common/Header.jsx
+++ src/components/RootView/common/Header.jsx
@@ -173,28 +173,6 @@ const Header = () => {
                 <hr />
               </SheetHeader>
                 <MyCart />
-              {/* <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input
-                    id="name"
-                    value="Pedro Duarte"
-                    className="col-span-3"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    Username
-                  </Label>
-                  <Input
-                    id="username"
-                    value="@peduarte"
-                    className="col-span-3"
-                  />
-                </div>
-              </div> */}
             </SheetContent>
           </Sheet>
 
