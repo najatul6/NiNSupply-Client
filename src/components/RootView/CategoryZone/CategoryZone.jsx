05c6fbf51384fdb60846e50cc5d8db7da7e8d7e6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-16 23:59:25 +0600

    update CategoryZone component to improve image sizing and layout consistency

--- src/components/RootView/CategoryZone/CategoryZone.jsx
+++ src/components/RootView/CategoryZone/CategoryZone.jsx
@@ -2,29 +2,35 @@ import Container from "@/components/common/Container";
 import { useEffect, useState } from "react";
 
 const CategoryZone = () => {
-    const [categories, setCategories] = useState([]);
-    useEffect(() => {
-        fetch('/packages.json')
-        .then(res=>res.json())
-        .then(data=>setCategories(data))
-    }, []);
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("/packages.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  
   return (
     <Container>
-        <h1 className="text-center text-2xl font-bold mt-4">Categories</h1>
-        <div className="grid grid-cols-3 gap-4">
-            {categories.map((category) => (
-            <div key={category.id} className="bg-[#f0f0f0] p-4 text-center">
-                <img
-                src={category.thumbnail}
-                alt={category.name}
-                className="w-20 h-20 mx-auto"
-                />
-                <p className="text-sm font-bold">{category.packageName}</p>
+      <h1 className="text-center text-2xl font-bold mt-4">Categories</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {categories.map((category) => (
+          <div key={category.id} className="bg-[#f0f0f0] text-center relative group overflow-hidden transition duration-300">
+            <img
+              src={category.thumbnail}
+              alt={category.packageName}
+              className="w-full h-full mx-auto object-cover group-hover:scale-125 transition duration-500"
+            />
+            <div
+              className="w-full h-full bg-background bg-blend-screen absolute inset-0 flex flex-col justify-center items-start bg-opacity-85" 
+            >
+              <h2 className="text-lg font-bold text-white">{category.packageName}</h2>
+              <p className="text-white">{category.description}</p>
             </div>
-            ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default CategoryZone
+export default CategoryZone;
