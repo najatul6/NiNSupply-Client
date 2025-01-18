7496c67a93a729edb375e05f80ef7c0816b134df
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-18 23:51:35 +0600

    refactor: remove unused imports and code from ProductPage component

--- src/pages/RootView/Shop/ProductPage.jsx
+++ src/pages/RootView/Shop/ProductPage.jsx
@@ -1,11 +1,1 @@
-import { useParams } from "react-router-dom"
-
-const ProductPage = () => {
-    const { name } = useParams()
-    console.log(name);
-  return (
-    <div>ProductPage</div>
-  )
-}
-
-export default ProductPage
+raf
