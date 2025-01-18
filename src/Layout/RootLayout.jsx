b11167aaa0bd451a35b2a1168d7eee1fee79a79b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-18 21:54:08 +0600

    feat: add Footer component with social icons and quote

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/RootView/common/Footer";
 import Header from "@/components/RootView/common/Header";
 import { Outlet } from "react-router-dom";
 
@@ -8,6 +9,7 @@ const RootLayout = () => {
       <div className="flex flex-col w-full">
         <Outlet />
       </div>
+      <Footer/>
     </div>
   );
 };
