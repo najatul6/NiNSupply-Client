baffd2de0c4655fc33b2c01a4dba5deeaf936c00
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-20 00:24:25 +0600

    feat: refactor PopularProducts component to utilize ShopItem for product display

--- src/components/RootView/Home/PopularProducts/PopularProducts.jsx
+++ src/components/RootView/Home/PopularProducts/PopularProducts.jsx
@@ -1,47 +1,19 @@
 import Container from "@/components/common/Container";
 import useProduct from "@/hooks/useProduct";
+import ShopItem from "../../Shop/ShopItem";
 
 const PopularProducts = () => {
   const [products] = useProduct();
   const popularProduct = products.filter(
     (product) => product.isPopular === true
   );
-  const exchangeRate = 120;
   return (
-    <Container className="">
-      <h2 className="text-2xl font-bold text-center mb-6">Popular Products</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {popularProduct.map((product) => (
-          <div
-            key={product.id}
-            className="bg-background2 rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300 flex flex-col hover:translate-y-[-5px]"
-          >
-            <img
-              src={product.thumbnail}
-              alt={product.productName}
-              className="w-full h-40 object-cover"
-            />
-            <div className="p-4 flex-grow">
-              <div className="flex justify-between">
-                
-                <h3 className="text-lg font-semibold mb-2">
-                  {product.productName}
-                </h3>
-                <p className="text-xl font-bold text-green-500">
-                ${ (product.price / exchangeRate).toFixed(2) }
-                </p>
-              </div>
-              <ul className="list-disc list-inside text-sm text-gray-500 marker:text-blue-500 mb-4">
-                {product.description.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
-            </div>
-            <button className="w-full bg-baseColor text-black py-2 text-center hover:bg-green-600 transition-colors duration-200">
-              Add to Cart
-            </button>
-          </div>
-        ))}
+    <Container>
+      <div className="space-y-6 my-10">
+        <h2 className="text-2xl font-bold text-center mb-6">
+          Popular Products
+        </h2>
+        <ShopItem item={popularProduct} />
       </div>
     </Container>
   );
