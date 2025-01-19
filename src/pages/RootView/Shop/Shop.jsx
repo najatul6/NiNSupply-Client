b4741b968b7838673f87dbc52f73f87fd47d8210
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-19 19:56:59 +0600

    feat: add Skeleton, Separator, and Tooltip components for enhanced UI elements

--- src/pages/RootView/Shop/Shop.jsx
+++ src/pages/RootView/Shop/Shop.jsx
@@ -3,6 +3,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 const Shop = () => {
   return (
     <div>
+      <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
       <Tabs defaultValue="account" className="">
         <TabsList className="">
           <TabsTrigger value="password">Password</TabsTrigger>
