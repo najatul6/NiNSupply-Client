173c303b26e22c9d43ae4dd65132d9689e173d68
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-20 01:02:19 +0600

    feat: integrate ShopItem into GiftCard component to display subscription products

--- src/pages/RootView/GiftCard/GiftCard.jsx
+++ src/pages/RootView/GiftCard/GiftCard.jsx
@@ -1,7 +1,17 @@
+import Container from "@/components/common/Container"
+import ShopItem from "@/components/RootView/common/ShopItem"
+import useProduct from "@/hooks/useProduct";
 
 const GiftCard = () => {
+  const [products] = useProduct();
+  const subscriptionProduct = products.filter(
+    (product) => product.category === "gift-card"
+  );
   return (
-    <div>GiftCard</div>
+    <Container>
+         <h2 className="text-2xl font-bold text-center mb-8">Subscriptions</h2>
+         <ShopItem item={subscriptionProduct}/>
+       </Container>
   )
 }
 
