69a506785544fa25d3bf1ddf8be342dbe408e542
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-23 21:38:47 +0600

    add Nav list for dashboard

---
+++ src/lib/navlist.js
@@ -0,0 +1,30 @@
+export const userNav=[
+    {
+        label:"Overview",
+        path:"dashboard/overview",
+        icon:""
+    },
+    {
+        label:"My Orders",
+        path:"dashboard/my-orders",
+        icon:""
+    }
+]
+
+export const AdminNav=[
+    {
+        label:"Overview",
+        path:"dashboard/overview",
+        icon:""
+    },
+    {
+        label:"Orders",
+        path:"dashboard/orders",
+        icon:""
+    },
+    {
+        label:"Users Control",
+        path:"users-control",
+        icon:""
+    }
+]
