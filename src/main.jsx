b5895349b05aa965ceb46d42dee1849d8d6d0688
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-29 21:38:28 +0600

    refactor: reorganize component structure in main.jsx for improved readability

--- src/main.jsx
+++ src/main.jsx
@@ -11,24 +11,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CartProvider } from "./providers/CartProvider";
 const queryClient = new QueryClient();
 
-
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <CartProvider>
       <QueryClientProvider client={queryClient}>
-        <HelmetProvider>
-          <RouterProvider router={Router} />
-          <ToastContainer
-            position="top-center"
-            autoClose={2000}
-            theme="colored"
-            transition={Bounce}
-          />
-        </HelmetProvider>
+        <CartProvider>
+          <HelmetProvider>
+            <RouterProvider router={Router} />
+            <ToastContainer
+              position="top-center"
+              autoClose={2000}
+              theme="colored"
+              transition={Bounce}
+            />
+          </HelmetProvider>
+        </CartProvider>
       </QueryClientProvider>
-      </CartProvider>
     </AuthProvider>
   </React.StrictMode>
 );
