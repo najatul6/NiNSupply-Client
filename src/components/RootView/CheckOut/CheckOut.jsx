acd82a93dda40de382516a5648d2f695d43c15a7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-03 22:37:07 +0600

    feat: add Bkash payment buttons to CheckOut component

--- src/components/RootView/CheckOut/CheckOut.jsx
+++ src/components/RootView/CheckOut/CheckOut.jsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
 
 const CheckOut = () => {
   return (
-    <div>CheckOut</div>
+    <div>
+      CheckOut
+      <Button>Pay with Bkash</Button>
+      <Button>Pay with Bkash</Button>
+      </div>
   )
 }
 
