6de27d576788f92943ace75e89178ed5da1dfbb8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-10 20:15:22 +0600

    feat: update CheckOut component layout for improved responsiveness and styling

--- src/components/RootView/CheckOut/CheckOut.jsx
+++ src/components/RootView/CheckOut/CheckOut.jsx
@@ -1,8 +1,8 @@
 const CheckOut = () => {
   return (
-    <div className="flex w-full min-h-screen">
-      <div className="hidden lg:flex justify-center items-center bg-black w-1/2 px-12">
-        <div className="max-w-md space-y-6 text-center text-primary-foreground">
+    <div className="flex flex-col-reverse lg:flex-row  w-full min-h-screen">
+      <div className="flex justify-center items-center bg-black lg:w-1/2 px-12">
+        <div className="max-w-md border-red-500 border space-y-6 text-center text-primary-foreground">
           <h1 className="text-4xl font-extrabold tracking-tight ">
             Something Special is waiting for you
           </h1>
@@ -11,9 +11,7 @@ const CheckOut = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col flex-1 justify-center items-center bg-gray-200 px-4 py-12 sm:px-6 lg:px-8">
-        <div className="lg:hidden"></div>
-      </div>
+      <div className="flex flex-col flex-1 justify-center items-center bg-gray-200 px-4 py-12 sm:px-6 lg:px-8"></div>
     </div>
   );
 };
