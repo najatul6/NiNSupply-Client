1ca34fbdf1c2c3bda70eea8457765acefd913115
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-12 02:29:56 +0600

    style: refactor AdminOverview to use TotalProducts component for product display

--- src/components/DashboardView/AdminOverview.jsx
+++ src/components/DashboardView/AdminOverview.jsx
@@ -1,17 +1,16 @@
 import useAllOrders from "@/hooks/useAllOrders";
 import useAllUser from "@/hooks/useAllUser";
-import useProduct from "@/hooks/useProduct";
-import { PackagePlus, Store, Truck, Users, WalletCards } from "lucide-react";
+import { PackagePlus, Truck, Users, WalletCards } from "lucide-react";
 import Chart from "react-google-charts";
 import TotalRevenue from "./TotalRevenue";
 import PendingRevenue from "./PendingRevenue";
 import ProcessingRevenue from "./ProcessingRevenue";
 import CompletedRevenue from "./CompletedRevenue";
+import TotalProducts from "./TotalProducts";
 
 const AdminOverview = () => {
   const [allUser] = useAllUser();
   const [allOrders] = useAllOrders();
-  const [products] = useProduct();
 
   const approvedOrders = allOrders?.filter(
     (order) => order?.status === "approved"
@@ -130,21 +129,7 @@ const AdminOverview = () => {
         </div>
 
         {/* Total Products */}
-        <div className="bg-gradient-to-b from-orange-200 to-orange-100 border-b-4 border-orange-500 rounded-lg shadow-xl p-5">
-          <div className="flex flex-row items-center">
-            <div className="flex-shrink pr-4">
-              <div className="rounded-full p-5 bg-orange-600">
-                <Store />
-              </div>
-            </div>
-            <div className="flex-1 text-right md:text-center">
-              <h2 className="font-bold uppercase text-gray-600">Products</h2>
-              <p className="font-bold text-3xl text-orange-600">
-                {products?.length}
-              </p>
-            </div>
-          </div>
-        </div>
+        <TotalProducts/>
       </section>
       <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div>
