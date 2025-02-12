817279e6c25e1dd884f47f0e7785464cf749bf4d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-12 22:05:19 +0600

    style: refactor AdminOverview by removing customer count display and add TotalCustomer component for better separation of concerns

--- src/components/DashboardView/AdminOverview.jsx
+++ src/components/DashboardView/AdminOverview.jsx
@@ -1,4 +1,3 @@
-import useAllOrders from "@/hooks/useAllOrders";
 import useAllUser from "@/hooks/useAllUser";
 import {  Users, WalletCards } from "lucide-react";
 import Chart from "react-google-charts";
@@ -11,8 +10,7 @@ import TotalOrders from "./TotalOrders";
 import NewOrders from "./NewOrders";
 
 const AdminOverview = () => {
-  const [allUser] = useAllUser();
-  const [allOrders] = useAllOrders();
+  
   const data = [
     ["Task", "Hours per Day"],
     ["Work", 9],
@@ -77,23 +75,7 @@ const AdminOverview = () => {
         </div>
 
         {/* Total Users */}
-        <div className="bg-gradient-to-b from-pink-300 to-pink-200 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
-          <div className="flex flex-row items-center">
-            <div className="flex-shrink pr-4">
-              <div className="rounded-full p-5 bg-pink-600">
-                <Users />
-              </div>
-            </div>
-            <div className="flex-1 text-right md:text-center">
-              <h2 className="font-bold uppercase text-gray-600">
-                Total Customers
-              </h2>
-              <p className="font-bold text-3xl text-pink-500">
-                {allUser?.length}
-              </p>
-            </div>
-          </div>
-        </div>
+       
 
         {/* Total Products */}
         <TotalProducts />
