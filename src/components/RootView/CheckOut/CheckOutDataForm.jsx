cba1f056d0407e15e8478301a95da33a74dfbced
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-13 23:44:44 +0600

    style: format CheckOutDataForm component for improved readability and layout

--- src/components/RootView/CheckOut/CheckOutDataForm.jsx
+++ src/components/RootView/CheckOut/CheckOutDataForm.jsx
@@ -1,12 +1,13 @@
-
-const CheckOutDataForm = ({totalPrice}) => {
+const CheckOutDataForm = ({ totalPrice }) => {
   return (
     <div className="min-h-screen flex justify-center items-center md:p-6 pattern">
-      <div>
-        
+      <div className="p-4 rounded-3xl shadow-2xl w-full max-w-2xl text-white">
+        <h2 className="text-2xl font-semibold text-center mb-6">
+          Billing and Contact Information
+        </h2>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CheckOutDataForm
+export default CheckOutDataForm;
