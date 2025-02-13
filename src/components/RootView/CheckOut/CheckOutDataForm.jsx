023c7203bc2b138772b7c1fdfbf138c63843884d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-13 23:48:14 +0600

    feat: integrate react-hook-form for form handling in CheckOutDataForm component

--- src/components/RootView/CheckOut/CheckOutDataForm.jsx
+++ src/components/RootView/CheckOut/CheckOutDataForm.jsx
@@ -1,12 +1,36 @@
 import PropTypes from "prop-types";
+import { useForm } from "react-hook-form"
 
 const CheckOutDataForm = ({ totalPrice }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
+  const onSubmit = (data) => console.log(data)
   return (
     <div className="min-h-screen flex justify-center items-center md:p-6 pattern">
       <div className="p-4 rounded-3xl shadow-2xl w-full max-w-2xl text-white">
         <h2 className="text-2xl font-semibold text-center mb-6">
           Billing and Contact Information
         </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("firstName", { required: true })}
+        aria-invalid={errors.firstName ? "true" : "false"}
+      />
+      {errors.firstName?.type === "required" && (
+        <p role="alert">First name is required</p>
+      )}
+
+      <input
+        {...register("mail", { required: "Email Address is required" })}
+        aria-invalid={errors.mail ? "true" : "false"}
+      />
+      {errors.mail && <p role="alert">{errors.mail.message}</p>}
+
+      <input type="submit" />
+    </form>
       </div>
     </div>
   );
