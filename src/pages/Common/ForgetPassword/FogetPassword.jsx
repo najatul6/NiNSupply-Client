e0de8dd54a2d9fd33e85b0bb8602e93b09377891
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-15 21:05:06 +0600

    fix: update styles in ForgotPassword component for improved UI consistency

--- src/pages/Common/ForgetPassword/FogetPassword.jsx
+++ src/pages/Common/ForgetPassword/FogetPassword.jsx
@@ -9,24 +9,25 @@ const ForgotPassword = () => {
   const {resetPassword}=useAuth()
 
 
-  const handleReset = async (e) => {
+  const handleReset = (e) => {
     e.preventDefault();
     setMessage("");
     setError("");
     setLoading(true);
 
-    try {
-      await resetPassword( email);
-      setMessage("Password reset email sent! Check your inbox.");
-    } catch (err) {
-      setError(err.message);
-    }
-    setLoading(false);
+    resetPassword(email)
+      .then(res=>{
+        console.log(res)
+      }).catch(err=>{
+        console.log(err)
+      })
+    //   setMessage("Password reset email sent! Check your inbox.");
+    
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+    <div className="flex items-center justify-center min-h-screen bg-gray-400">
+      <div className="bg-gray-500 p-6 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-4 text-center">Reset Password</h2>
         <form onSubmit={handleReset} className="space-y-4">
           <input
@@ -35,7 +36,7 @@ const ForgotPassword = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full px-4 py-2 text-baseColor bg-transparent border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <button
             type="submit"
