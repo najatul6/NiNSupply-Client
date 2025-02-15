9ad532a9c3d33820b7b3f1576e0817e6027f6551
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-15 23:25:58 +0600

    fix: format code and improve readability in DashboardHeader component

--- src/components/DashboardView/common/DashboardHeader.jsx
+++ src/components/DashboardView/common/DashboardHeader.jsx
@@ -3,21 +3,21 @@ import headerLogo from "../../../assets/ninSupply.svg";
 import useAuth from "@/hooks/useAuth";
 
 const DashboardHeader = () => {
-  const {user}=useAuth()
+  const { user } = useAuth();
   return (
     <header className="flex shadow-md py-1 px-4 sm:px-7 bg-background2 min-h-[70px] tracking-wide z-[110] fixed top-0 w-full">
       <div className="flex flex-wrap items-center justify-between gap-4 w-full relative bg-background2">
-        <Link
-          to="/"
-          className="flex justify-center items-center gap-2"
-        >
+        <Link to="/" className="flex justify-center items-center gap-2">
           <img src={headerLogo} alt="logo" className="w-36" />
           {/* <p className="text-3xl font-merienda font-bold">Admin Panel</p> */}
         </Link>
 
         <Link to="/dashboard/my-profile">
           <img
-            src={user?.photoURL || "https://cdn.pixabay.com/photo/2019/08/11/18/59/icon-4399701_1280.png"}
+            src={
+              user?.photoURL ||
+              "https://cdn.pixabay.com/photo/2019/08/11/18/59/icon-4399701_1280.png"
+            }
             alt="profile-pic"
             className="w-10 h-10 rounded-full border-2 border-baseColor cursor-pointer"
           />
