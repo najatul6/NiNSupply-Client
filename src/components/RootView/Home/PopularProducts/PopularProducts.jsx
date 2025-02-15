c4778b57e7716ece62ff34d89034e409df6453a9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-15 22:02:12 +0600

    fix: enhance loading animation in PopularProducts component with motion effects

--- src/components/RootView/Home/PopularProducts/PopularProducts.jsx
+++ src/components/RootView/Home/PopularProducts/PopularProducts.jsx
@@ -17,11 +17,13 @@ const PopularProducts = () => {
       </h2>
       <Container>
         {isLoading ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          <motion.div initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.1, delay: index * 0.1 }} className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {Array.from({ length: 8 }).map((_, index) => (
               <ProductCardSkeleton key={index} />
             ))}
-          </div>
+          </motion.div>
         ) : (
           <ShopItem item={popularProduct} />
         )}
